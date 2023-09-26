cc8fdb1b9e0cac9980ccb22f2ec504bcabb3cec3
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-09-27 00:37:12 +0500

    Made an api to get a specific blog

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,14 @@ app.get("/blogs", (req, res) => {
   res.status(200).json(allBlogs);
 });
 
+//get specific blog
+app.get("/blogs/:id", (req, res) => {
+  const blogId = parseInt(req.params.id);
+  const requestedBlog = allBlogs.find((blog) => blog.id === blogId);
+  console.log(requestedBlog);
+  res.status(200).json({ requestedBlog });
+});
+
 app.listen(4000, () => {
   console.log("Server is running at port 4000");
 });
